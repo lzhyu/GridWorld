ff6de0b1ecc3c822190d6e52a3bfa6e95ec1d0b7
lzy <zhengyua17@mails.tsinghua.edu.cn>
2021-08-07 15:49:04 +0800

    add gitignore, test_render untracked, add basic check for all envs

---
+++ basic_test.py
@@ -0,0 +1,28 @@
+# to run basic test for all envs
+import inspect
+from gridworld.utils.wrapper.wrappers import ImageInputWarpper
+from gridworld.utils.test_util import *
+import gridworld
+
+import os
+import importlib
+from gridworld.envs.fourrooms import FourroomsBase
+
+path = os.path.dirname(__file__)
+envs_path = os.path.join(path, 'gridworld', 'envs')
+
+envfiles = [ f for f in os.listdir(envs_path) if os.path.isfile(os.path.join(envs_path,f))]
+envfiles = [f.split('.')[0] for f in envfiles if f.endswith('.py')]
+
+for f in envfiles:
+    module = importlib.import_module('gridworld.envs.'+ f )
+    for name, obj in inspect.getmembers(module, inspect.isclass):
+        try:
+            if isinstance(obj(), FourroomsBase):
+                env = ImageInputWarpper(obj())
+                check_render(env)
+                check_run(env)
+                print(f"basic check for {name} finished")
+        except Exception as e:
+            pass
+
