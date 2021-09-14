a265ee33b509b3fc723534fca43f4b6c984ff79b
1Ruby <rubyscm0417@gmail.com>
2021-09-14 22:12:21 +0800

    Update gym register

--- gridworld/envs/__init__.py
+++ gridworld/envs/__init__.py
@@ -6,3 +6,34 @@ from .fourrooms_gate import FourroomsGate
 from .ninerooms import NineroomsBase
 from .ninerooms_gate import NineroomsGate
 from .ninerooms_gate_v2 import NineroomsGateV2
+from gym.envs.registration import register
+
+register(
+    id='fourrooms_base-v0',
+    entry_point='gridworld.envs:FourroomsBase'
+)
+
+register(
+    id='fourrooms_coin-v0',
+    entry_point='girdworld.envs:FourroomsCoin'
+)
+
+register(
+    id='fourrooms_water-v0',
+    entry_point='girdworld.envs:FourroomsWater'
+)
+
+register(
+    id='ninerooms_base-v0',
+    entry_point='gridworld.envs:NineroomsBase'
+)
+
+register(
+    id='ninerooms_gate-v1',
+    entry_point='gridworld.envs:NineroomsGate'
+)
+
+register(
+    id='ninerooms_gate-v2',
+    entry_point='gridworld.envs:NineroomsGateV2'
+)
