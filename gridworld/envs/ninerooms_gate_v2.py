812aaf98046a836dcf0596351f7488fc58f10daa
1Ruby <rubyscm0417@gmail.com>
2021-09-14 22:19:13 +0800

    Add gate_class & easy_env, color_render()

--- gridworld/envs/ninerooms_gate_v2.py
+++ gridworld/envs/ninerooms_gate_v2.py
@@ -1,17 +1,25 @@
 """
 Ninerooms Game with gates -- v2
 All gates are in one color, whose type is given by the description.
+
+gate_class:
+    'all': whole 12 gates,
+    '8gates': 8 gates
+    '6gates': 6 gates
+    '4gates': 4 gates
+    'no_gate': no gate
+diag:
+    True: init_pos and goal are in diagonal rooms
+    False: init_pos and goal are in different rooms
+easy_env: affect train_model_gen()
+    'no_easy': default config where true model can be learned
+    'one_env': only one env in training set, gate_class must be 'all'
+    'cross_type': each gate_class has different types
+    'no_pair': each pair gates are different in >=1 env
 """
 
 from .ninerooms_gate import *
 from ..utils.env_utils.ninerooms_util import *
-
-random_train_model = train_model_gen
-def random_test_model():
-    Model = dict()
-    for gate in gates_pos:
-        Model[gate] = np.random.choice(['coin', 'water', 'wall'])
-    return Model
 
 class NineroomsGateV2State(NineroomsGateState):
     def __init__(self, position_n, current_steps, goal_n, done, num_pos, gatedict, cum_reward: list, descr=None):
@@ -33,10 +41,24 @@ class NineroomsGateV2State(NineroomsGateState):
 
 
 class NineroomsGateV2(NineroomsGate):
-    def __init__(self, Model=None, max_epilen=100, init_pos=None, goal=None, seed=None, mode='train'):
-        super().__init__(Model=Model, max_epilen=max_epilen, init_pos=init_pos, goal=goal, seed=seed, mode=mode,
-                         easy_dy=False, easy_env=False, fix_pos=False)
+    def __init__(self, Model=None, max_epilen=100, init_pos=None, goal=None, gate_class='all', easy_env='no_easy',
+                 diag=False, seed=None, mode='train'):
+        super(NineroomsGateV2, self).__init__(Model=Model, max_epilen=max_epilen, init_pos=init_pos, goal=goal,
+                                              seed=seed, mode=mode, easy_dy=False, easy_env=False, fix_pos=False)
         self.state = NineroomsGateV2State.frombase(self.state)
+        self.gate_class = gate_class
+        self.diag = diag
+        self.easy_env = easy_env
+        self.init_gatedict = self.init_gate()
+        
+    def init_gate(self):
+        init_gatedict = dict()
+        for gate_index in gate_group_index[self.gate_class]:
+            init_gatedict[gates_pos[gate_index]] = True
+        for gate in gates_pos:
+            if not init_gatedict.get(gate, False):
+                init_gatedict[gate] = False
+        return init_gatedict
 
     def to_descr(self):
         if self.Model is None or len(self.Model) == 0:
@@ -45,37 +67,62 @@ class NineroomsGateV2(NineroomsGate):
         gate_list.sort(key=lambda ele: ele[0])
         descr = [v for (k, v) in gate_list]
         return descr
-
+    
+    def model_gen(self):
+        if self.mode != 'train':
+            return test_model_gen(self.gate_class)
+        elif self.easy_env == 'one_env':
+            return easy_model_gen(self.gate_class)
+        elif self.easy_env == 'cross_type':
+            return easy_model_gen_type(self.gate_class)
+        elif self.easy_env == 'no_pair':
+            return easy_model_gen_group(self.gate_class)
+        else:
+            return train_model_gen(self.gate_class)
+    
+    def _one_random_pos(self, pos_n):
+        room = -1
+        rooms = list(range(9))
+        rooms.remove(4)
+        for i in rooms:
+            if pos_n in rooms_pos[i]:
+                room = i
+                break
+        if room == -1:
+            raise Exception(f'Position {pos_n} cannot be initial position or goal.')
+        if self.diag:
+            if room not in [0, 2, 6, 8]:
+                raise Exception(f'Position {pos_n} cannot be initial position or goal in diagonal-init env.')
+            _room = 8 - room
+            return np.random.choice(rooms_pos[_room])
+        rooms.remove(room)
+        _room = np.random.choice(rooms)
+        return np.random.choice(rooms_pos[_room])
+        
+    def _init_pos_goal(self):
+        if self.init_random and self.goal_random:
+            if self.diag:
+                init_rooms = [0, 2, 6, 8]
+            else:
+                init_rooms = [0, 1, 2, 3, 5, 6, 7]
+            init_room = np.random.choice(init_rooms)
+            init_pos_n = np.random.choice(rooms_pos[init_room])
+            return init_pos_n, self._one_random_pos(init_pos_n)
+        elif self.init_random:
+            return self._one_random_pos(self.goal), self.goal
+        elif self.goal_random:
+            return self.init_pos, self._one_random_pos(self.init_pos)
+        else:
+            return self.init_pos, self.goal
+        
     def reset(self):
         self.open = True
 
-        if self.mode == 'train' and self.model_random:
-            self.Model = random_train_model()
-        elif self.model_random:
-            self.Model = random_test_model()
+        if self.model_random:
+            self.Model = self.model_gen()
+        init_pos_n, goal_n = self._init_pos_goal()
 
-        init_rooms = [0, 1, 2, 3, 5, 6, 7, 8]
-
-        if self.init_random:
-            init_room = np.random.choice(init_rooms)
-            init_pos_n = np.random.choice(rooms_pos[init_room])
-        else:
-            init_pos_n = self.init_pos
-            init_room = -1
-            for i in range(len(rooms_pos)):
-                if init_pos_n in rooms_pos[i]:
-                    init_room = i
-                    break
-        if init_room in init_rooms:
-            init_rooms.remove(init_room)
-
-        if self.goal_random:
-            goal_room = np.random.choice(init_rooms)
-            goal_n = np.random.choice(rooms_pos[goal_room])
-        else:
-            goal_n = self.goal
-
-        self.state = NineroomsGateV2State(init_pos_n, 0, goal_n, False, self.num_pos, deepcopy(init_gatedict), [], self.to_descr())
+        self.state = NineroomsGateV2State(init_pos_n, 0, goal_n, False, self.num_pos, deepcopy(self.init_gatedict), [],  self.to_descr())
         return self.state.to_obs()
 
     def step(self, action):
@@ -86,8 +133,8 @@ class NineroomsGateV2(NineroomsGate):
         self.state.current_steps += 1
 
         next_pos = self.basic_step(self.state.position_n, self.directions[action])
-        if next_pos in gates_pos:
-            if self.Model[next_pos] == 'coin' and self.state.gatedict[next_pos]:
+        if next_pos in gates_pos and self.state.gatedict[next_pos]:
+            if self.Model[next_pos] == 'coin':
                 reward = 1
                 self.state.gatedict[next_pos] = False
             elif self.Model[next_pos] == 'water':
@@ -122,9 +169,21 @@ class NineroomsGateV2(NineroomsGate):
         blocks.extend(self.make_basic_blocks())
         arr = self.render_with_blocks(self.origin_background, blocks)
         return arr
-
-if __name__ == '__main__':
-    # basic test
-    env_origin = ImageInputWarpper(NineroomsGateV2())
-    check_render(env_origin)
-    check_run(env_origin)
+    
+    def color_render(self):
+        blocks = []
+        for gate in gates_pos:
+            if self.state.gatedict.get(gate, None):
+                x, y = self.tocell[gate]
+                if self.Model[gate] == 'coin':
+                    color = (1, 1, 0)
+                elif self.Model[gate] == 'water':
+                    color = (0, 1, 0)
+                else:
+                    color = (1, 0, 1)
+                blocks.append(self.make_block(x, y, color))
+        blocks.extend(self.make_basic_blocks())
+        arr = self.render_with_blocks(self.origin_background, blocks)
+        return arr
+        
+        
